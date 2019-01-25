e9e111d1934b051634a6b5fa96329d0ac5d335d7
Jesús Tejada Pérez <tejadong@gmail.com>
2019-01-25 17:29:02 +0100

    Commit inicial

---
+++ app.js
@@ -0,0 +1,23 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+            .then(tabla => console.log(tabla))
+            .catch(e => console.log(e));
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log('Archivo creado: ', colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
+
+// console.log(argv);
---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,56 @@
+// requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite) => {
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${ base } no es un número`);
+            return;
+        }
+
+        console.log('================'.green);
+        console.log(`= Tabla del ${ base } =`.green);
+        console.log('================'.green);
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${ base } * ${ i } = ${ base * i }\n`;
+        }
+
+        resolve(data);
+
+    })
+}
+
+crearArchivo = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${ base } no es un número`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${ base } * ${ i } = ${ base * i }\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, data, (err) => {
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla-${ base }-al-${ limite }.txt`)
+        })
+
+    })
+
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
